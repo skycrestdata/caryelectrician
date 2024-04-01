e6ac706d3705ba425a4ed11ff90f717708ab1263
skycrestdata <skycrestdatamarketing@gmail.com>
2024-04-01 11:56:59 -0400

    Added functionality to form submit, needs a way to slow down submits

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,4 @@
-'use strict';
-
-
+"use strict";
 
 /**
  * add event listener on multiple elements
@@ -10,9 +8,7 @@ const addEventOnElements = function (elements, eventType, callback) {
   for (let i = 0, len = elements.length; i < len; i++) {
     elements[i].addEventListener(eventType, callback);
   }
-}
-
-
+};
 
 /**
  * NAVBAR TOGGLE FOR MOBILE
@@ -26,11 +22,9 @@ const toggleNavbar = function () {
   navbar.classList.toggle("active");
   overlay.classList.toggle("active");
   document.body.classList.toggle("nav-active");
-}
+};
 
 addEventOnElements(navTogglers, "click", toggleNavbar);
-
-
 
 /**
  * HEADER
@@ -47,16 +41,13 @@ window.addEventListener("scroll", function () {
   }
 });
 
-
-
 /**
  * SLIDER
  */
 
 const sliders = document.querySelectorAll("[data-slider]");
 
-const initSlider = function(currentSlider) {
-
+const initSlider = function (currentSlider) {
   const sldierContainer = currentSlider.querySelector("[data-slider-container]");
   const sliderPrevBtn = currentSlider.querySelector("[data-slider-prev]");
   const sliderNextBtn = currentSlider.querySelector("[data-slider-next]");
@@ -65,7 +56,7 @@ const initSlider = function(currentSlider) {
 
   const moveSliderItem = function () {
     sldierContainer.style.transform = `translateX(-${sldierContainer.children[currentSlidePos].offsetLeft}px)`;
-  }
+  };
 
   /**
    * NEXT SLIDE
@@ -81,7 +72,7 @@ const initSlider = function(currentSlider) {
     }
 
     moveSliderItem();
-  }
+  };
 
   sliderNextBtn.addEventListener("click", slideNext);
 
@@ -89,8 +80,7 @@ const initSlider = function(currentSlider) {
    * PREVIOUS SLIDE
    */
 
-   const slidePrev = function () {
-
+  const slidePrev = function () {
     if (currentSlidePos <= 0) {
       currentSlidePos = sldierContainer.childElementCount - 1;
     } else {
@@ -98,7 +88,7 @@ const initSlider = function(currentSlider) {
     }
 
     moveSliderItem();
-  }
+  };
 
   sliderPrevBtn.addEventListener("click", slidePrev);
 
@@ -107,12 +97,11 @@ const initSlider = function(currentSlider) {
     sliderNextBtn.style.display = "none";
     sliderPrevBtn.style.display = "none";
   }
+};
 
+for (let i = 0, len = sliders.length; i < len; i++) {
+  initSlider(sliders[i]);
 }
-
-for (let i = 0, len = sliders.length; i < len; i++) { initSlider(sliders[i]); }
-
-
 
 /**
  * ACCORDION
@@ -123,7 +112,6 @@ const accordions = document.querySelectorAll("[data-accordion]");
 let lastActiveAccordion = accordions[0];
 
 const initAccordion = function (currentAccordion) {
-
   const accordionBtn = currentAccordion.querySelector("[data-accordion-btn]");
 
   const expandAccordion = function () {
@@ -134,10 +122,11 @@ const initAccordion = function (currentAccordion) {
     currentAccordion.classList.toggle("expanded");
 
     lastActiveAccordion = currentAccordion;
-  }
+  };
 
   accordionBtn.addEventListener("click", expandAccordion);
+};
 
+for (let i = 0, len = accordions.length; i < len; i++) {
+  initAccordion(accordions[i]);
 }
-
-for (let i = 0, len = accordions.length; i < len; i++) { initAccordion(accordions[i]); }
